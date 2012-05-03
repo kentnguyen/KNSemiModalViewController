026959049ad0277d7010056ad32c9302e7b9a9c6
Kent Nguyen <nguyen.dmz@gmail.com>
2012-05-03 09:36:11 +0800

    More instruction

--- KNSemiModalViewControllerDemo/KNThirdViewController.m
+++ KNSemiModalViewControllerDemo/KNThirdViewController.m
@@ -18,8 +18,9 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  self.view.backgroundColor = [UIColor whiteColor];
+  // You can customize your own semi-modal size
   self.view.frame = CGRectMake(0, 0, 320, 180);
+  self.view.backgroundColor = [UIColor whiteColor];
 
   UILabel * demoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,15, 320, 50)];
   demoLabel.text = @"You can add any UIView elements here\neven dismiss button\n(if you know what you are doing)";
@@ -28,10 +29,12 @@
   demoLabel.textAlignment = UITextAlignmentCenter;
   [self.view addSubview:demoLabel];
   
+  // Here's how to call dismiss button on the parent ViewController
+  // be careful with view hierarchy
   UIButton * dismissButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
   [dismissButton setTitle:@"Dismiss" forState:UIControlStateNormal];
   dismissButton.frame = CGRectMake(100, 75, 120, 35);
-  [dismissButton addTarget:self.parentViewController // careful with view hierarchy
+  [dismissButton addTarget:self.parentViewController
                     action:@selector(dismissSemiModalView)
           forControlEvents:UIControlEventTouchUpInside];
   [self.view addSubview:dismissButton];
