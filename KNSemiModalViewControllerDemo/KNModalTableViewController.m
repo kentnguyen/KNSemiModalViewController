24253671fda1ee22dff2abb7f994f550a354c8d8
Kent Nguyen <nguyen.dmz@gmail.com>
2012-05-04 09:41:53 +0800

    Missing code in commit

--- KNSemiModalViewControllerDemo/KNModalTableViewController.m
+++ KNSemiModalViewControllerDemo/KNModalTableViewController.m
@@ -17,7 +17,7 @@
 - (id)initWithStyle:(UITableViewStyle)style {
   self = [super initWithStyle:style];
   if (self) {
-    self.view.frame = CGRectMake(0, 0, 320, 150);
+    self.view.frame = CGRectMake(0, 0, 320, 200);
   }
   return self;
 }
@@ -25,11 +25,11 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-  return 4;
+  return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return 5;
+  return 10;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -38,13 +38,14 @@
   if (!cell) {
     cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
   }
-  cell.textLabel.text = [NSString stringWithFormat:@"Row in modal table %d", indexPath.row];
+  cell.textLabel.text = [NSString stringWithFormat:@"Crazy shit %d", indexPath.row];
   return cell;
 }
 
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
