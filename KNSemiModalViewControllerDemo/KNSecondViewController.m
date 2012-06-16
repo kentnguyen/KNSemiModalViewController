e62c46078ca5991ab618c876c663375580b52eb3
Kent Nguyen <nguyen.dmz@gmail.com>
2012-06-16 16:14:13 +0800

    Copyright

--- KNSemiModalViewControllerDemo/KNSecondViewController.m
+++ KNSemiModalViewControllerDemo/KNSecondViewController.m
@@ -3,7 +3,7 @@
 //  KNSemiModalViewControllerDemo
 //
 //  Created by Kent Nguyen on 2/5/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Kent Nguyen. All rights reserved.
 //
 
 #import "KNSecondViewController.h"
