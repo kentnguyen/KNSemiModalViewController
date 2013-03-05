9fbfd7be0c909cb307fdecbd8cf7850aed8ea5d5
Kent Nguyen <nguyen.dmz@gmail.com>
2013-03-05 23:11:04 +0800

    More comprehensive code for storing keys

--- Source/NSObject+YMOptionsAndDefaults.m
+++ Source/NSObject+YMOptionsAndDefaults.m
@@ -7,20 +7,20 @@
 
 @implementation NSObject (YMOptionsAndDefaults)
 
-#define kYMStandardOptionsTableName @"YMStandardOptionsTableName"
-#define kYMStandardDefaultsTableName @"YMStandardDefaultsTableName"
+static char const * const kYMStandardOptionsTableName = "YMStandardOptionsTableName";
+static char const * const kYMStandardDefaultsTableName = "YMStandardDefaultsTableName";
 
 - (void)ym_registerOptions:(NSDictionary *)options
 				  defaults:(NSDictionary *)defaults
 {
-	objc_setAssociatedObject(self, (__bridge const void *)(kYMStandardOptionsTableName), options, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-	objc_setAssociatedObject(self, (__bridge const void *)(kYMStandardDefaultsTableName), defaults, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	objc_setAssociatedObject(self, kYMStandardOptionsTableName, options, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	objc_setAssociatedObject(self, kYMStandardDefaultsTableName, defaults, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 - (id)ym_optionOrDefaultForKey:(NSString*)optionKey
 {
-	NSDictionary *options = objc_getAssociatedObject(self, (__bridge const void *)(kYMStandardOptionsTableName));
-	NSDictionary *defaults = objc_getAssociatedObject(self, (__bridge const void *)(kYMStandardDefaultsTableName));
+	NSDictionary *options = objc_getAssociatedObject(self, kYMStandardOptionsTableName);
+	NSDictionary *defaults = objc_getAssociatedObject(self, kYMStandardDefaultsTableName);
 	NSAssert(defaults, @"Defaults must have been set when accessing options.");
 	return options[optionKey] ?: defaults[optionKey];
 }
